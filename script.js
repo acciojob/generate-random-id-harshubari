13d35d0a2c5c46c19d66f675d2e80a91cc3cdcd4
harshubari <121845828+harshubari@users.noreply.github.com>
2024-02-28 16:04:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+let regexpr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 function makeid(l) {
-  // write your code here
-}
+	let len = regexpr.length;
+	let res = ' ';
+	for ( let i = 0; i < len; i++ ) {
+        res += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+	return res;
+} 
 
 // Do not change the code below.
+
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l)); 
