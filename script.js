37533bf2fb0e3b7de2addc3f5f89ce8a04d5d807
harshubari <121845828+harshubari@users.noreply.github.com>
2024-02-28 16:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 function makeid(l) {
+	if(l == null || l == "") return ;
 	let len = characters.length;
 	let res = ' ';
 	for ( let i = 0; i < len; i++ ) {
         res += characters.charAt(Math.floor(Math.random() * len));
     }
 
-	let newres = res.slice(0, l+1)
+	let newres = res.slice(0, l)
 	console.log(newres);
 	return newres;
 } 
